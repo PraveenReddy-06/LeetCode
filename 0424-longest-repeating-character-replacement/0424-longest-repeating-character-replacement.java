e119f75506c95903d41066b8416ad2214db74141
Praveen Reddy <2400032662@kluniversity.in>
2025-12-16 21:34:12 +0530

    Optimize characterReplacement using frequency array
    Refactored characterReplacement method to use an array for frequency counting instead of a HashMap.

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,6 +1,31 @@
 class Solution {
     public int characterReplacement(String s, int k) {
 
+        int[] freq = new int[26];
+        int left = 0, maxFreq = 0, result = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            int idx = s.charAt(right) - 'A';
+            freq[idx]++;
+            maxFreq = Math.max(maxFreq, freq[idx]);
+
+            while ((right - left + 1) - maxFreq > k) {
+                freq[s.charAt(left) - 'A']--;
+                left++;
+            }
+
+            result = Math.max(result, right - left + 1);
+        }
+
+        return result;
+    }
+}
+
+
+/*class Solution {
+    public int characterReplacement(String s, int k) {
+
         HashMap<Character, Integer> map = new HashMap<>();
         int left = 0, maxFreq = 0, result = 0;
 
@@ -19,4 +44,4 @@ class Solution {
         }
         return result;
     }
-}
+}*/
