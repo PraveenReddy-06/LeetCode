85f5d5c0c0dea2fa469e64690ccb2cc94eae5c1d
Praveen Reddy <2400032662@kluniversity.in>
2026-01-14 22:46:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String removeKdigits(String num, int k) {
+        Stack<Integer> stack = new Stack<>();
+        String arr="";
+        for(int i=0;i<num.length();i++){
+            while(!stack.isEmpty() && num.charAt(stack.peek()) >= num.charAt(i) && k>0){
+                stack.pop();
+                k--;
+            }
+            stack.push(i);
+        }
+        while(k>0){
+            stack.pop();
+            k--;
+        }
+        int i=0;
+        while(!stack.isEmpty()){
+            arr = num.charAt(stack.pop())+ arr;
+        }
+        arr = arr.replaceFirst("^0+","");
+        if(arr.length() == 0) return "0";
+        return arr;
+    }
+}
