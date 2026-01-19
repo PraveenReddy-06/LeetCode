44247ebdb443087d5d7dcf1464f2747029656332
Praveen Reddy <2400032662@kluniversity.in>
2026-01-19 20:15:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,26 +1,21 @@
 class Solution {
-    public int maxProduct(int[] arr) {
+    public int maxProduct(int[] nums) {
+        int prefix=1;
+        int sufix=1;
+        int max=Integer.MIN_VALUE;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                prefix=1;
+            }
+            if(nums[n-i-1]==0){
+                sufix=1;
+            }
+            prefix=prefix*nums[i];
+            sufix *= nums[n-i-1];
 
-        if (arr == null || arr.length == 0) return 0;
-
-        int minSoFar = arr[0];   // minimum product ending at current index
-        int maxSoFar = arr[0];   // maximum product ending at current index
-        int answer = arr[0];     // final maximum product
-
-        for (int i = 1; i < arr.length; i++) {
-
-            int current = arr[i];
-
-            int option1 = current;
-            int option2 = current * minSoFar;
-            int option3 = current * maxSoFar;
-
-            maxSoFar = Math.max(option1, Math.max(option2, option3));
-            minSoFar = Math.min(option1, Math.min(option2, option3));
-
-            answer = Math.max(answer, maxSoFar);
+            max=Math.max(max,Math.max(prefix,sufix));
         }
-
-        return answer;
+        return max;
     }
 }
