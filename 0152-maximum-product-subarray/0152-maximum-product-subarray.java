b2374bcc96776dd9f6588bdf8895aa020580f2b8
Praveen Reddy <2400032662@kluniversity.in>
2026-01-28 15:54:37 +0530

    Time: 1 ms (90.55%), Space: 47.4 MB (44.26%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,9 +1,9 @@
 class Solution {
     public int maxProduct(int[] nums) {
+        int n= nums.length;
         int prefix=1;
         int sufix=1;
         int max=Integer.MIN_VALUE;
-        int n=nums.length;
         for(int i=0;i<n;i++){
             if(prefix==0){
                 prefix=1;
@@ -11,9 +11,8 @@ class Solution {
             if(sufix==0){
                 sufix=1;
             }
-            prefix=prefix*nums[i];
+            prefix *= nums[i];
             sufix *= nums[n-i-1];
-
             max=Math.max(max,Math.max(prefix,sufix));
         }
         return max;
