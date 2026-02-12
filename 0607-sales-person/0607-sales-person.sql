f9bd97c1d42d9105957ca30c6b283be659c128eb
Praveen Reddy <2400032662@kluniversity.in>
2026-02-12 22:29:45 +0530

    Time: 1444 ms (34.04%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,9 +1,8 @@
-SELECT name
-FROM SalesPerson
-WHERE sales_id NOT IN (
-    SELECT o.sales_id
-    FROM Orders o
-    JOIN Company c
-    ON o.com_id = c.com_id
-    WHERE c.name = 'RED'
-);
+select s.name
+from SalesPerson s
+where s.name not in
+    (select s.name
+    from SalesPerson s
+        left join Orders o on s.sales_id = o.sales_id
+        left join Company c on o.com_id = c.com_id
+    where c.name = 'Red')
