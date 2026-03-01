b20ff7e32d81023997b425a139d271da34c0a368
Praveen Reddy <2400032662@kluniversity.in>
2026-03-01 23:30:57 +0530

    Time: 544 ms (59.58%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,7 +1,6 @@
-SELECT 
-ROUND(
-    COUNT(DISTINCT a.player_id) 
-    / (SELECT COUNT(DISTINCT player_id) FROM Activity),
+SELECT ROUND(
+    COUNT(DISTINCT a.player_id) /
+    (SELECT COUNT(DISTINCT player_id) FROM Activity),
     2
 ) AS fraction
 FROM Activity a
@@ -9,6 +8,6 @@ JOIN (
     SELECT player_id, MIN(event_date) AS first_login
     FROM Activity
     GROUP BY player_id
-) f
-ON a.player_id = f.player_id
-AND a.event_date = DATE_ADD(f.first_login, INTERVAL 1 DAY);
+) t
+ON a.player_id = t.player_id
+WHERE DATEDIFF(a.event_date, t.first_login) = 1;
