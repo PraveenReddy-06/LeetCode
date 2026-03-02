3a9f2102bb5603143ff684a4ac9e66834b114d1d
Praveen Reddy <2400032662@kluniversity.in>
2026-03-02 20:19:51 +0530

    Time: 619 ms (75.48%), Space: 0B (100%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,9 @@
+select d.name as Department,e.name as Employee,e.salary as Salary
+from employee e
+join department d
+on e.departmentid = d.id
+where (e.departmentid,e.salary) in (
+    select departmentId,max(salary)
+    from employee
+    group by departmentId
+);
