f74b84dbaf70575571ab6bbcea7e9807db3a37b8
Praveen Reddy <2400032662@kluniversity.in>
2026-03-06 10:58:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -0,0 +1,17 @@
+(select name as result 
+from users 
+join movierating using(user_id)
+group by user_id 
+order by count(movie_id) desc, name
+limit 1
+)
+union all
+
+(select title as result
+from movies
+join movierating using (movie_id)
+where created_at >="2020-02-01" and created_at<"2020-03-01"
+group by movie_id,title
+order by avg(rating) desc,title
+limit 1;)
+ 
